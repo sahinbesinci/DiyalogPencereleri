f4873590a124e257dfabbed45a0904a1b22461f8
Sahin <sahinbesinci@gmail.com>
2016-11-30 04:07:46 +0300

    update to ProgressDialog

--- app/src/main/java/com/learning/sahin/diyalogpenceresi/Activities/Main.java
+++ app/src/main/java/com/learning/sahin/diyalogpenceresi/Activities/Main.java
@@ -3,7 +3,9 @@ package com.learning.sahin.diyalogpenceresi.Activities;
 import android.app.DialogFragment;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
@@ -21,7 +23,13 @@ import com.learning.sahin.diyalogpenceresi.R;
  */
 
 public class Main extends FragmentActivity implements View.OnClickListener{
-    Button btnBasicDialog,btnAlertDialog,btnAlert2Dialog,btnEmbeddingDialog;
+    Button btnBasicDialog,btnAlertDialog,btnAlert2Dialog,btnEmbeddingDialog,btnProgressDialog;
+
+    ProgressDialog progressBar;
+    private int progressBarStatus = 0;
+    private Handler progressBarHandler = new Handler();
+    private long fileSize = 0;
+
     static int mStackLevel = 0;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -102,10 +110,12 @@ public class Main extends FragmentActivity implements View.OnClickListener{
         btnAlertDialog = (Button) findViewById(R.id.btnAlertDialog);
         btnAlert2Dialog = (Button) findViewById(R.id.btnAlert2Dialog);
         btnEmbeddingDialog = (Button) findViewById(R.id.btnEmbeddingDialog);
+        btnProgressDialog = (Button) findViewById(R.id.btnProgressDialog);
         btnBasicDialog.setOnClickListener(this);
         btnAlertDialog.setOnClickListener(this);
         btnAlert2Dialog.setOnClickListener(this);
         btnEmbeddingDialog.setOnClickListener(this);
+        btnProgressDialog.setOnClickListener(this);
     }
 
     @Override
@@ -124,7 +134,68 @@ public class Main extends FragmentActivity implements View.OnClickListener{
             case R.id.btnEmbeddingDialog:
                 SelectingBetweenDialogOrEmbedding();
                 break;
+            case R.id.btnProgressDialog:
+                ProgressDialogShow();
             default:break;
         }
+    }
+
+    private void ProgressDialogShow() {
+        // prepare for a progress bar dialog
+        progressBar = new ProgressDialog(this);
+        progressBar.setCancelable(true);
+        progressBar.setMessage("File downloading ...");
+        progressBar.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        progressBar.setProgress(0);
+        progressBar.setMax(100);
+        progressBar.show();
+        //reset progress bar status
+        progressBarStatus = 0;
+        //reset filesize
+        fileSize = 0;
+        new Thread(new Runnable() {
+            public void run() {
+                while (progressBarStatus < 100) {
+                    // process some tasks
+                    progressBarStatus = doSomeTasks();
+                    // your computer is too fast, sleep 1 second
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    // Update the progress bar
+                    progressBarHandler.post(new Runnable() {
+                        public void run() {
+                            progressBar.setProgress(progressBarStatus);
+                        }
+                    });
+                }
+                // ok, file is downloaded,
+                if (progressBarStatus >= 100) {
+                    // sleep 2 seconds, so that you can see the 100%
+                    try {
+                        Thread.sleep(2000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    // close the progress bar dialog
+                    progressBar.dismiss();
+                }
+            }
+        }).start();
+    }
+    public int doSomeTasks() {
+        while (fileSize <= 1000000) {
+            fileSize++;
+            if (fileSize == 100000) {
+                return 10;
+            } else if (fileSize == 200000) {
+                return 20;
+            } else if (fileSize == 300000) {
+                return 30;
+            }
+        }
+        return 100;
     }
 }
